7ad3551fd0efb6f835ec1f1a87ceb23171bb01da
VladislavaFn <vladislava.fridmann1@aol.com>
2025-08-06 21:34:57 +0300

    hw7

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,5 @@
+from logger_masks import logger
+
 def get_mask_card_number(card_number: str) -> str | None:
     """Маскирует номер банковской карты. Формат: ХХХХ ХХ** **** ХХХХ"""
     if len(card_number) != 16 or not card_number.isdigit():
@@ -10,3 +12,15 @@ def get_mask_account(account_number: str) -> str:
     if len(account_number) != 20 or not account_number.isdigit():
         raise ValueError("Номер счета должен быть строкой из 20 цифр")
     return f"**{account_number[-4:]}"
+
+
+def test_masks_log():
+    logger.debug("Функция из masks запущена")
+    try:
+        # логика
+        logger.debug("Функция успешно выполнена")
+    except Exception as e:
+        logger.error(f"Ошибка: {e}")
+
+if __name__ == "__main__":
+    test_masks_log()
